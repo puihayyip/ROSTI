13030a25cb11be2ec54e72a05060e0e6055e99d6
puihayyip <puihay.yip@gmail.com>
2022-06-14 18:01:25 +0800

    Setting up server

---
+++ Backend/server.js
@@ -0,0 +1,17 @@
+const { urlencoded } = require("express");
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+require("dotenv").config();
+const PORT = process.env.PORT || 3000;
+
+app.use(morgan("dev"));
+app.use(urlencoded({ extended: true }));
+
+app.get("/api/", (req, res) => {
+  res.send("Hello restaurant owners");
+});
+
+app.listen(PORT, (req, res) => {
+  console.log(`Listening to port ${PORT}`);
+});
--- README.md
+++ README.md
@@ -1,4 +1,1 @@
-This is the right one
-
-
-Testing a difference
+#Rosti
