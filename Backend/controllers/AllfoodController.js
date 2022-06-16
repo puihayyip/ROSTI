e10599658e161af97d5510cc9510a8bc7419a6f9
Jun Kiat <junkiatang@gmail.com>
2022-06-17 00:29:24 +0800

    add users and allfood seed data

--- Backend/controllers/AllfoodController.js
+++ Backend/controllers/AllfoodController.js
@@ -1,11 +1,25 @@
+// /api/allfood
+
 const express = require("express");
 const Allfood = require("../models/Allfood");
 const { StatusCodes } = require("http-status-codes");
+const allfoodseed = require("../models/allfoodseed")
 
 const router = express.Router();
 
+//! Seed Route (reset database)
+router.get("/seed", async (req, res) => {
+	try {
+		await Allfood.deleteMany({});
+		const newAllfood = await Allfood.create(allfoodseed);
+		console.log(newAllfood);
+	} catch (error) {
+		res.send(error);
+	}
+});
+
 router.get("/", async (req, res) => {
-	res.send("At allfoodcontroller");
+	res.send("At AllfoodController");
 });
 
 module.exports = router;
