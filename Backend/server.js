dab7562b69ef1ec6fc3621e106aa38800033887f
Jun Kiat <junkiatang@gmail.com>
2022-06-16 21:03:48 +0800

    change server settings to 4000 for backend

--- Backend/server.js
+++ Backend/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 require("dotenv").config();
-const PORT = process.env.PORT || 1000;
+const PORT = process.env.PORT || 4000;
 
 app.use(morgan("dev"));
 app.use(urlencoded({ extended: true }));
