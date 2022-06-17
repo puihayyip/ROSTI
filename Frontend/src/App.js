f4767d8c03632f39ab06aff4a4995adc543230c0
liyic25 <liyic25@gmail.com>
2022-06-17 10:19:12 +0800

    test Cashier view
    try frontend /cashier

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -5,6 +5,7 @@ import { useState, createContext } from "react";
 import UserSelection from "./pages/UserSelection";
 import Login from "./pages/Login";
 import Order from "./pages/OrderTabComponents/Order";
+import CashierView from "./pages/CashierComponents/CashierView"
 
 export const stateContext = createContext();
 
@@ -26,6 +27,8 @@ function App() {
             </Route>
 
             <Route path="/superSecurePath" element={<Order />} />
+            <Route path="/cashier" element={<CashierView />} />
+
           </Routes>
         </BrowserRouter>
       </stateContext.Provider>
