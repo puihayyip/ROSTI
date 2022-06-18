2034ccf79246bc4f6c7bbb15e56df03a21748a4c
liyic25 <liyic25@gmail.com>
2022-06-18 21:11:05 +0800

    Update ViewMainCashier

--- Frontend/src/pages/CashierComponents/ViewMainCashier.jsx
+++ Frontend/src/pages/CashierComponents/ViewMainCashier.jsx
@@ -1,13 +1,43 @@
 import Head from "../GeneralComponents/MainHeader";
-import { Link } from "react-router-dom";
+import {Link} from 'react-router-dom'
+
+
+const handleClick = () => {
+    console.log("say heyyyyy")
+//     fetch(`http://localhost:7000/api/orders/`, {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({ ...item }),
+//     })
+//       .then((response) => response.json())
+//       .then((data) => {
+//         console.log(data);
+//         // replaceHoliday(data.data);
+//       });
+  };
+//   const item = data ?? {} ;
+
 function ViewMainCashier() {
-  return (
-    <>
-      <Head />
-      <h1>Which Table Bill do you want to see?</h1>
-      <Link to="/tablebill">Table 34</Link>
-    </>
-  );
+    return(
+        <>
+        <Head/>
+        <h1>Which Table Bill do you want to see?</h1>
+        <ul>
+
+        <li>
+            <Link to="/tablebill" onClick={handleClick}>Table 34</Link>
+            </li>
+        <li>
+            <Link to="/tablebill">Table 44</Link>
+            </li>
+
+        </ul>
+
+       
+        </>
+    )
 }
 
 export default ViewMainCashier;
