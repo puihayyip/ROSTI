cbe0cc9835e77dd70fa512deee596bdba66eb8bc
liyic25 <liyic25@gmail.com>
2022-06-18 21:26:30 +0800

    Create trueCooked.jsx

---
+++ Frontend/src/pages/KitchenComponents/trueCooked.jsx
@@ -0,0 +1,16 @@
+// import Stack from "@mui/material/Stack";
+// import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import SendIcon from "@mui/icons-material/Send";
+
+export default function trueCooked(){
+
+    return(
+
+    <Button
+    variant="contained"
+    color="error"
+    endIcon={<SendIcon />}> Done? </Button>
+
+    )
+}
