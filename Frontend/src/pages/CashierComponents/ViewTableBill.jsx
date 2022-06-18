00cbf74c2f890772f7c7f8659467fe8789a95bff
liyic25 <liyic25@gmail.com>
2022-06-18 21:09:58 +0800

    updated ViewTableBill

--- Frontend/src/pages/CashierComponents/ViewTableBill.jsx
+++ Frontend/src/pages/CashierComponents/ViewTableBill.jsx
@@ -1,43 +1,32 @@
 import Head from "../GeneralComponents/MainHeader";
 import * as React from "react";
-import Stack from "@mui/material/Stack";
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import SendIcon from "@mui/icons-material/Send";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import CompFinalOrderList from "./CompFinalOrderList";
 import CompEditOrderList from "./CompEditOrderList";
-import { useNavigate } from "react-router-dom";
-
-const handleEdit = () => {
-  console.log("Time to Edit EVERYTHING!!!");
-};
+import CompFinalOrderButtons from "./CompFinalOrderButtons";
+import CompEditOrderButtons from "./CompEditOrderButtons";
 
 export default function TablePreviewBill() {
+  const [edit, setEdit]= useState(false)
+
   const nav = useNavigate();
   const handlePayment = () => {
-    // console.log ("MAKE A LOT OF MONEY!!!")
     nav("/receipt");
+  }
+  const handleConfirm = () => {
+    setEdit(!edit)
   };
+const handleEdit = () => {
+  setEdit(!edit)
+};
+
   return (
     <>
       <Head />
-      {/* <CompFinalOrderList/> */}
-      <CompEditOrderList handleEdit={handleEdit} />
-      <Box m={1} display="flex" justifyContent="center" alignItems="center">
-        <Stack direction="row" spacing={2}>
-          {/* <Button variant="outlined" color="error" onClick= {handleEdit}>
-            Edit Bill
-          </Button> */}
-          <Button
-            variant="contained"
-            color="success"
-            endIcon={<SendIcon />}
-            onClick={handlePayment}
-          >
-            Proceed to Payment
-          </Button>
-        </Stack>
-      </Box>
+      {edit=== true? <CompEditOrderList/> : <CompFinalOrderList />}
+      {edit=== true? <CompEditOrderButtons handleConfirm={handleConfirm}/> : <CompFinalOrderButtons handlePayment={handlePayment} handleEdit={handleEdit}/>}
+
     </>
   );
 }
