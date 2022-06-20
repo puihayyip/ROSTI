eb886c6a4dd63894baf07c12f0c75f8987ac0129
liyic25 <liyic25@gmail.com>
2022-06-20 19:50:40 +0800

    CompFinalOrderList logic complete

--- Frontend/src/pages/CashierComponents/CompFinalOrderList.jsx
+++ Frontend/src/pages/CashierComponents/CompFinalOrderList.jsx
@@ -1,133 +1,122 @@
-import { styled } from '@mui/material/styles';
-import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell, { tableCellClasses } from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-
-// function priceRow() {
-//   return qty * {item.quantity};
-// }
-
-// function createRow() {
-//   const price = priceRow(qty, unit);
-//   return { desc, qty, unit, price };
-// }
-
-// interface Row {
-//   desc: string;
-//   qty: number;
-//   unit: number;
-//   price: number;
-// }
-
-// function subtotal() {
-//   return items.map(({ price }) => price).reduce((sum, i) => sum + i, 0);
-// }
-
-// // const rows = [
-// //   createRow('Paperclips (Box)', 100, 1.15),
-// //   createRow('Paper (Case)', 10, 45.99),
-// //   createRow('Waste Basket', 2, 17.99),
-// // ];
-
-
-////////////////////////
-
+import { styled } from "@mui/material/styles";
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
-    [`&.${tableCellClasses.head}`]: {
-      backgroundColor: theme.palette.common.black,
-      color: theme.palette.common.white,
-      fontWeight: 'bold' 
-    },
-    [`&.${tableCellClasses.body}`]: {
-      fontSize: 16,
-      fontWeight: 'bold' 
-    },
-  }));
-  
-  function CompFinalOrderList({order}) {
-    // console.log(order)
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+    fontWeight: "bold",
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+}));
 
-    const TAX_RATE = 0.07;
+function CompFinalOrderList({ order }) {
+  // console.log(order)
 
-    // function ccyFormat(num) {
-    //   return `${num.toFixed(2)}`;
-    // }
-    const x = 5.5 //{item.price}
+  function ccyFormat(num) {
+    return `${num.toFixed(2)}`;
+  }
+  const x = 5.5; //{item.price}
 
-    // const invoiceItemSubtotal = (item) => x * item.quantity;
-    // const invoiceTotalSubtotal=() => {
-    //   items.map((item) => (x* item.quantity));
-    // } 
-    // const invoiceTaxes = TAX_RATE * invoiceTotalSubtotal;
-    // const invoiceTotal = invoiceTaxes + invoiceTotalSubtotal;
+  //? SUBTOTAL
+  const arrayItemSubTotal = [];
 
-    return(
+  order?.orders?.map((obj, index) =>
+    obj.items.map((item) => arrayItemSubTotal.push(item.quantity * x))
+  );
+
+  console.log("arraySub", arrayItemSubTotal);
+
+  let SubTotal=0
+  for (let i=0; i<arrayItemSubTotal.length; i++) {
+     SubTotal = SubTotal + arrayItemSubTotal[i] 
+  }
+  console.log('SubTotal', SubTotal)
+
+  //? DISCOUNT
+  let DiscountRate = 0.1
+  let DiscountAmt = DiscountRate * SubTotal
+
+  //? TAX
+  let TaxRate = 0.07;
+  let TaxAmt= (SubTotal - DiscountAmt)*TaxRate
+
+  //? TOTAL
+  let TotalAmt = SubTotal - DiscountAmt + TaxAmt
+
+  return (
     <>
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 700, maxWidth: 900 }} align="center" aria-label="spanning table">
-        <TableHead>
-          <TableRow>
-            {/* <TableCell align="center" colSpan={3}>
+      <TableContainer component={Paper}>
+        <Table
+          sx={{ minWidth: 700, maxWidth: 900 }}
+          align="center"
+          aria-label="spanning table"
+        >
+          <TableHead>
+            <TableRow>
+              {/* <TableCell align="center" colSpan={3}>
               Details
             </TableCell>
             <TableCell align="right">Price</TableCell> */}
-          </TableRow>
-          <TableRow>
-            <StyledTableCell>Item Name</StyledTableCell>
-            <StyledTableCell align="right">Item Price</StyledTableCell>
-            <StyledTableCell align="right">Quantity</StyledTableCell>
-            <StyledTableCell align="right">Sub-total</StyledTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-
-          {order?.orders?.map((obj, index) => (
-            // console.log(obj)
-            obj.items.map((item) =>
-              // console.log(item)
-            <TableRow key ={index}> 
-              <TableCell>{item.foodID}</TableCell>
-              <TableCell align="right">{x}</TableCell>
-              <TableCell align="right">{item.quantity}</TableCell>
-              <TableCell align="right">{x * item.quantity}</TableCell>
             </TableRow>
-            )
-            ))}
+            <TableRow>
+              <StyledTableCell>Item Name</StyledTableCell>
+              <StyledTableCell align="right">Item Price</StyledTableCell>
+              <StyledTableCell align="right">Quantity</StyledTableCell>
+              <StyledTableCell align="right">Sub-total</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {order?.orders?.map((obj, index) =>
+              // console.log(obj)
+              obj.items.map((item) => (
+                // console.log(item)
+                <TableRow key={index}>
+                  <TableCell>{item.foodID}</TableCell>
+                  <TableCell align="right">{x}</TableCell>
+                  <TableCell align="right">{item.quantity}</TableCell>
+                  <TableCell align="right">{x * item.quantity}</TableCell>
+                </TableRow>
+              ))
+            )}
 
-          <TableRow>
-            <TableCell rowSpan={4} />
-            <StyledTableCell colSpan={2}>Subtotal</StyledTableCell>
-            <TableCell align="right">ccyFormat</TableCell>
-          </TableRow>
+            <TableRow>
+              <TableCell rowSpan={4} />
+              <StyledTableCell colSpan={2}>Subtotal</StyledTableCell>
+              <TableCell align="right">{ccyFormat(SubTotal)}</TableCell>
+            </TableRow>
 
-          <TableRow>
-            <StyledTableCell>Discounts</StyledTableCell>
-            <TableCell align="right">??</TableCell>
-            <TableCell align="right">!!!</TableCell>
-          </TableRow>
+            <TableRow>
+              <StyledTableCell>Discounts</StyledTableCell>
+              <TableCell align="right">{`${(DiscountRate*100).toFixed(0)}%`}</TableCell>
+              <TableCell align="right">{ccyFormat(DiscountAmt)}</TableCell>
+            </TableRow>
 
-          <TableRow>
-            <StyledTableCell>Tax</StyledTableCell>
-            <TableCell align="right">{`${(TAX_RATE * 100).toFixed(0)} %`}</TableCell>
-            <TableCell align="right">ccyFormat</TableCell>
-          </TableRow>
+            <TableRow>
+              <StyledTableCell>Tax</StyledTableCell>
+              <TableCell align="right">{`${(TaxRate * 100).toFixed(0)}%`}</TableCell>
+              <TableCell align="right">{ccyFormat(TaxAmt)}</TableCell>
+            </TableRow>
 
-          <TableRow>
-            <StyledTableCell colSpan={2}>TOTAL</StyledTableCell>
-            <TableCell align="right">ccyFormat</TableCell>
-          </TableRow>
-
-        </TableBody>
-      </Table>
-    </TableContainer>
+            <TableRow>
+              <StyledTableCell colSpan={2}>TOTAL</StyledTableCell>
+              <TableCell align="right">{ccyFormat(TotalAmt)}</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
     </>
   );
 }
 
-export default CompFinalOrderList
+export default CompFinalOrderList;
